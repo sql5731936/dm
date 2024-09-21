c9361c631f3ef1fc450714103253f41174abf372
sql5731936 <kimi20244@hotmail.com>
2024-09-21 23:06:12 +0300

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -7,9 +7,9 @@
 
 
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','https://sql5731936.github.io/dm/');
+if(!defined('base_url')) define('base_url','http://localhost/dms/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
-//if(!defined('dev_data')) define('dev_data',$dev_data);
+if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"sql8.freemysqlhosting.net");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"sql8732324");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"SSV3BDX3dz");
